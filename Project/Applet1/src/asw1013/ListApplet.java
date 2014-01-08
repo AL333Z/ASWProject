f3789316018713a7ec358dd46fb58559cd934b1d
mattibal <mattibal@gmail.com>
2014-01-08 18:50:26 +0100

    partial implementation of the tweet list applet and comet webservice

--- Project/Applet1/src/asw1013/ListApplet.java
+++ Project/Applet1/src/asw1013/ListApplet.java
@@ -2,10 +2,7 @@ package asw1013;
 
 import javax.swing.JApplet;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.xml.bind.JAXBContext;
 import org.w3c.dom.*;
 
 /**
@@ -14,82 +11,89 @@ import org.w3c.dom.*;
  */
 public class ListApplet extends JApplet {
 
-    JLabel l = new JLabel();
-    JButton b = new JButton("send req");
-
     HTTPClient hc = new HTTPClient();
+    ManageXML mngXML;
     boolean logged = false;
-
-    JAXBContext jc = null;
+    
+    int lastDownloadedTweet = 0;
 
     public void init() {
 
         try {
             hc.setSessionId(getParameter("sessionId"));
             hc.setBase(getDocumentBase());
+            mngXML = new ManageXML();
             
-            SwingUtilities.invokeAndWait(new Runnable() {
-                public void run() {
-
-                    Container cp = getContentPane();
-                    cp.setLayout(new GridLayout(3, 2));
-
-                    cp.add(l);
-                    cp.add(b);
-                    
-                    b.addActionListener(new ActionListener() {
-
-                        @Override
-                        public void actionPerformed(ActionEvent e) {
-                            try {
-                                ManageXML mngXML = new ManageXML();
-                                String str = "listatweet: \r\n";
-
-                                // sample request for all tweets
-                                Document data = mngXML.newDocument();
-                                Element rootReq = data.createElement("tweetsrequest");
-                                rootReq.appendChild(data.createTextNode("")); // TODO put start and stop num of tweets to get (pagination)
-                                data.appendChild(rootReq);
-
-                                Document answer = hc.execute("tweets", data);
-                                
-                                NodeList tweetsList = answer.getElementsByTagName("tweets");
-                                for (int i = 0; i < tweetsList.getLength(); i++) {
-                                    Element tweetElem = (Element) tweetsList.item(i);
-                                    // TODO add data from this tweetElem to the swing UI
-                                    // example:
-                                    str = str + tweetElem.getElementsByTagName("message").item(0).getTextContent() + "\r\n";
-                                }
-
-                                l.setText(str);
-
-                                // sample request for all users
-//                                Document data2 = mngXML.newDocument();
-//                                Element rootReq2 = data2.createElement("registration");
-//
-//                                rootReq2.appendChild(data2.createTextNode("")); // TODO put start and stop num of users to get (pagination)
-//                                data2.appendChild(rootReq2);
-//
-//                                str += "\r\nUsers:\r\n";
-//                                answer = hc.execute("users", data2);
-//
-//                                str += "\r\n " + answer.toString() + "\r\n";
-
-                                l.setText(str);
-
-                                //t.setText("");
-                            } catch (Exception ex) {
-                                ex.printStackTrace();
-                                l.setText(ex.getMessage());
-                            }
-                        }
-                    });
-
+            Container cp = getContentPane();
+            cp.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+            
+            
+            new SwingWorker<Void, NodeList>(){
+                @Override
+                protected Void doInBackground() throws Exception {
+                    while(true){
+                        NodeList tweets = getTweets();
+                        publish(tweets);
+                        waitForUpdate();
+                    }
                 }
-            });
+
+                @Override
+                protected void process(java.util.List<NodeList> chunks) {
+                    NodeList tweetsList = chunks.get(0);
+
+                    // TODO Add tweets to the UI
+                    for (int i = 0; i < tweetsList.getLength(); i++) {
+                        Element tweetElem = (Element) tweetsList.item(i);
+                                    // TODO add data from this tweetElem to the swing UI
+                        // example:
+                        tweetElem.getElementsByTagName("message").item(0).getTextContent();
+                    }
+                }
+                
+            }.execute();
+            
         } catch (Exception e) {
-            e.printStackTrace();
-            l.setText(e.getMessage());
+
+        }
+    }
+    
+    
+    private NodeList getTweets() throws Exception {
+        
+        // prepare the request xml
+        Document data = mngXML.newDocument();
+        Element rootReq = data.createElement("tweetsrequest");
+        Element op = data.createElement("operation");
+        op.appendChild(data.createTextNode("getText"));
+        rootReq.appendChild(op);
+        Element startTweetElem = data.createElement("startTweet");
+        startTweetElem.appendChild(data.createTextNode(lastDownloadedTweet+""));
+        rootReq.appendChild(startTweetElem);
+        data.appendChild(rootReq);
+
+        Document answer = hc.execute("tweets", data);
+
+        NodeList tweetsList = answer.getElementsByTagName("tweets");
+        return tweetsList;
+    }
+    
+    
+    private void waitForUpdate() {
+        try {
+
+            // prepare the request xml
+            Document data = mngXML.newDocument();
+            Element rootReq = data.createElement("tweetsrequest");
+            Element op = data.createElement("operation");
+            op.appendChild(data.createTextNode("waitForUpdate"));
+            rootReq.appendChild(op);
+            data.appendChild(rootReq);
+            
+            Document answer = hc.execute("tweets", data);
+
+        } catch (Exception e) {
+
         }
     }
 
--- Project/WebApplication/src/java/asw1013/TweetListService.java
+++ Project/WebApplication/src/java/asw1013/TweetListService.java
@@ -2,7 +2,12 @@ package asw1013;
 
 import asw1013.util.TweetListFile;
 import asw1013.entity.TweetList;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.LinkedList;
+import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,38 +15,105 @@ import javax.servlet.http.HttpSession;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 /**
  * Service from which a client can get a list of tweets
  */
 
-@WebServlet(urlPatterns = {"/tweets"})
+@WebServlet(urlPatterns = {"/tweets"}, asyncSupported=true)
 public class TweetListService extends AbstractXmlServiceServlet{
+    
+    private LinkedList<AsyncContext> contexts = new LinkedList<AsyncContext>();
 
     @Override
     protected void operations(Document data, HttpSession session, 
             HttpServletRequest request, HttpServletResponse response, 
             ManageXML mngXML) throws Exception {
        
-        Element recvRoot = data.getDocumentElement();
-        // TODO get from XML the start and stop numbers of tweets to sent (pagination)
+        String operation = data.getElementsByTagName("operation").item(0).getTextContent();
+        
+        if(operation.equals("getTweets")){
+            
+            // TODO get from XML the start and stop numbers of tweets to sent (pagination)
+            
+            TweetListFile tweetFile = new TweetListFile();
+            TweetList tweetList = tweetFile.readFile();
 
-        TweetListFile tweetFile = new TweetListFile();
-        TweetList tweetList = tweetFile.readFile();
-       
-        // TODO filter from the tweetlist only the tweets that we want to send to the client
-        TweetList tweetListToSend = tweetList; //only for example
+            // TODO filter from the tweetlist only the tweets that we want to send to the client
+            TweetList tweetListToSend = tweetList; //only for example
+
+            JAXBContext jc = JAXBContext.newInstance(TweetList.class);
+            Marshaller marsh = jc.createMarshaller();
+            Document doc = mngXML.newDocument();
+            marsh.marshal(tweetListToSend, doc);
+
+            OutputStream os = response.getOutputStream();
+            mngXML.transform(os, doc);
+            os.close();
+            
+            
+        } else if (operation.equals("postTweet")){
+            
+            // TODO store the new tweet in the db
+            
+            
+            
+            // Notify the event to listeners
+            synchronized (this) {
+                for (AsyncContext asyncContext : contexts) {
+                    OutputStream aos = asyncContext.getResponse().getOutputStream();
+                    // don't send anything to the client
+                    aos.close();
+                    asyncContext.complete();
+                }
+                contexts.clear();
+            }
+            
+        } else if(operation.equals("waitForUpdate")) {
+            
+            // This will stop the transmission of the HTTP response until a timeout occurs
+            // or somebody will "unlock" this request (when somebody posts a new tweet)
+
+            AsyncContext asyncContext = request.startAsync();
+
+            asyncContext.setTimeout(10 * 1000);
+            asyncContext.addListener(new AsyncListener() {
+                @Override
+                public void onTimeout(AsyncEvent e) throws IOException {
+                    AsyncContext asyncContext = e.getAsyncContext();
+                    boolean confirm;
+                    synchronized (this) {
+                        if ((confirm = contexts.contains(asyncContext))) {
+                            contexts.remove(asyncContext);
+                        }
+                    }
+                    if (confirm) {
+                        OutputStream tos = asyncContext.getResponse().getOutputStream();
+                        // don't send anything to the client
+                        tos.close();
+                        asyncContext.complete();
+                    }
+                }
+
+                @Override
+                public void onComplete(AsyncEvent event) throws IOException {
+                }
+
+                @Override
+                public void onError(AsyncEvent event) throws IOException {
+                }
+
+                @Override
+                public void onStartAsync(AsyncEvent event) throws IOException {
+                }
+
+            });
+            synchronized (this) {
+                contexts.add(asyncContext);
+            }
+
+        }
         
-        JAXBContext jc = JAXBContext.newInstance(TweetList.class);
-        Marshaller marsh = jc.createMarshaller();
-        Document doc = mngXML.newDocument();
-        marsh.marshal(tweetListToSend, doc);
-       
-        
-        OutputStream os = response.getOutputStream();
-        mngXML.transform(os, doc);
-        os.close();
         
     }
     
